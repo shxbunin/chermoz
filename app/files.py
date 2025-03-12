5b4edd32fb6f8f4540eb9a22f57e96c21091ecc0
Igor Shabunin <shabunin181104@gmail.com>
2025-03-12 09:54:39 +0500

    fix paths

--- app/files.py
+++ app/files.py
@@ -1,16 +1,17 @@
 ﻿from pathlib import Path
 import uuid
 
-upload_dir = Path("images")
+app_dir = Path("app")
+upload_dir = Path("gallery")
 static_dir = Path("static")
-
 
 def get_unique_filename(file_name):
     ext = Path(file_name).suffix
     unique_filename = uuid.uuid4().hex + ext
     return unique_filename
+
 def save_file(file_content, file_name):
     name = get_unique_filename(file_name)
-    file_path = static_dir / upload_dir / name
+    file_path = app_dir / static_dir / upload_dir / name
     file_path.write_bytes(file_content)
     return str((upload_dir / name).as_posix())
