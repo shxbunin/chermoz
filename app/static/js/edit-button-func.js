40fee9e80431e57ddf179d8991399b1791420cf2
Igor Shabunin <shabunin181104@gmail.com>
2025-03-19 22:17:27 +0500

    fix

--- app/static/js/edit-button-func.js
+++ app/static/js/edit-button-func.js
@@ -1,40 +1,50 @@
-﻿function toggleEditing() {
-  const desc = document.getElementById('desc');
-  const descEdit = document.getElementById('desc-edit');
-  const textarea = descEdit.querySelector('textarea');
-  const editButton = document.getElementById('editButton');
+function toggleEditing() {
+    const desc = document.getElementById('desc');
+    const descEdit = document.getElementById('desc-edit');
+    const editButton = document.getElementById('editButton');
+    const textarea = document.getElementById('desc-textarea');
 
-  if (editButton.innerText === 'Редактировать') {
-    descEdit.value = desc.innerText.trim();
-    desc.style.display = 'none';
-    descEdit.style.display = 'block';
-    editButton.innerText = 'Сохранить';
-  } else {
-    const newText = textarea.value
-    console.log(newText +  " aboba")
-    editDesc(editButton, newText)
-    desc.innerText = newText;
-    desc.style.display = 'block';
-    descEdit.style.display = 'none';
-    editButton.innerText = 'Редактировать';
-  }
+    if (desc.style.display === 'block') {
+        desc.style.display = 'none';
+        descEdit.style.display = 'block';
+        editButton.innerText = 'Сохранить';
+
+    autoResize(textarea);
+
+    } else {
+        const newText = textarea.value
+        editDesc(editButton, newText)
+        desc.innerText = newText;
+        desc.style.display = 'block';
+        descEdit.style.display = 'none';
+        editButton.innerText = 'Редактировать';
+    }
 }
-document.getElementById('editButton').onclick = toggleEditing;
 
+function autoResize(el) {
+    el.style.height = 'auto';
+    el.style.height = el.scrollHeight + 10 + 'px';
+}
+
+const textarea = document.getElementById('desc-textarea');
+
+textarea.addEventListener('input', () => { autoResize(textarea); });
 
 async function editDesc(btn, desc) {
-  const photoId = btn.dataset.photoId;
+    const photoId = btn.dataset.photoId;
 
-  try {
-    const res = await fetch('/edit_desc', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ id: photoId, desc: desc})
-    });
+    try {
+        const res = await fetch('/edit_desc', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id: photoId, desc: desc})
+        });
+        const { status, result } = await res.json();
 
-    const { status, result } = await res.json();
-    console.log('Ответ сервера:', status, result);
-  } catch (err) {
-    console.error('Ошибка отправки:', err);
-  }
+        console.log('Ответ сервера:', status, result);
+    } catch (err) {
+        console.error('Ошибка отправки:', err);
+    }
 }
+
+document.getElementById('editButton').onclick = toggleEditing;
